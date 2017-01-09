74baaf0a615342846386ea33b8f83e45ecab66b6
softprops <d.tangren@gmail.com>
2017-01-09 14:29:14 -0500

    handle process signals for graceful shutdown. a.k.a docker stop

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,17 @@ extern crate hyper;
 #[macro_use]
 extern crate log;
 extern crate jira_transit;
+#[macro_use]
+extern crate chan;
+extern crate chan_signal;
 
+use chan_signal::Signal;
+use std::process::exit;
 use afterparty::Hub;
 use hyper::Server;
 use jira_transit::{Config, DefaultGithub, DefaultJira, Transit};
 
-fn run(config: Config) {
+fn run(_sdone: chan::Sender<()>, config: Config) {
     let github_secret = config.github_secret.clone();
     let github = DefaultGithub::new(hyper::Client::new(), config.clone());
     let jira = DefaultJira::new(hyper::Client::new(), config.clone());
@@ -30,7 +35,21 @@ fn run(config: Config) {
 fn main() {
     env_logger::init().unwrap();
     match envy::from_env::<Config>() {
-        Ok(config) => run(config),
+        Ok(config) => {
+            let signal = chan_signal::notify(&[Signal::INT, Signal::TERM]);
+            let (sdone, rdone) = chan::sync(0);
+            // start the application in a thread
+            ::std::thread::spawn(move || run(sdone, config));
+            // wait for process signal or application to stop
+            chan_select! {
+                signal.recv() -> signal => {
+                    println!("received signal: {:?}", signal)
+                },
+                rdone.recv() => {
+                    println!("Program completed normally.")
+                }
+            }
+        },
         Err(envy::Error::MissingValue(field)) => {
             panic!("missing required env var {}",
                    field.to_owned().to_uppercase())
